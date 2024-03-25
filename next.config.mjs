abdef33a240819016968e6c2f4604964f7e785b8
joshfgohunt <98337772+joshfgohunt@users.noreply.github.com>
2024-03-25 10:01:46 -0700

    I may be at a significant crossroads regarding rewrites
    It seems I either get to use those (currently helping with displaying projects) or I get to export the project as a whole to Github Pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,12 @@ const nextConfig = {
   basePath: "/Personal_Website",
   output: "export", // <=== enables static exports
   reactStrictMode: true,
+  images: {
+    unoptimized: true // https://github.com/vercel/next.js/issues/40240
+  },
   async rewrites() {
     return [
+      // /https://nextjs.org/docs/messages/export-no-custom-routes
       { source: "/project_1", destination: "/projects/Project_1_movies.html" },
       { source: "/project_2", destination: "/projects/Project_2_dunedin.html" },
       { source: "/project_3", destination: "/projects/Project_3_mtg.html" },
