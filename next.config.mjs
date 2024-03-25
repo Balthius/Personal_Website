5d0605300c552b4ce64f93bae139d6a4321006bf
joshfgohunt <98337772+joshfgohunt@users.noreply.github.com>
2024-03-25 10:38:30 -0700

    Trying to add rewrites back in following a successful deploy

--- next.config.mjs
+++ next.config.mjs
@@ -5,20 +5,20 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true // https://github.com/vercel/next.js/issues/40240
+  },
+  async rewrites() {
+    return [
+      // /https://nextjs.org/docs/messages/export-no-custom-routes
+      { source: "/project_1", destination: "/projects/Project_1_movies.html" },
+      { source: "/project_2", destination: "/projects/Project_2_dunedin.html" },
+      { source: "/project_3", destination: "/projects/Project_3_mtg.html" },
+      {
+        source: "/project_4",
+        destination: "/projects/Project_4_Stroop.html"
+      },
+      { source: "/project_5", destination: "/projects/Project_5_Enron.html" }
+    ];
   }
-  //   async rewrites() {
-  //     return [
-  //       // /https://nextjs.org/docs/messages/export-no-custom-routes
-  //       { source: "/project_1", destination: "/projects/Project_1_movies.html" },
-  //       { source: "/project_2", destination: "/projects/Project_2_dunedin.html" },
-  //       { source: "/project_3", destination: "/projects/Project_3_mtg.html" },
-  //       {
-  //         source: "/project_4",
-  //         destination: "/projects/Project_4_Stroop.html"
-  //       },
-  //       { source: "/project_5", destination: "/projects/Project_5_Enron.html" }
-  //     ];
-  //   }
 };
 
 export default nextConfig;
